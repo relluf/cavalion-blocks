a49fbaad32d07e1bae6ed2f8fbd12ff81eb1c464
Ralph Kazemier <ralph.kazemier@gmail.com>
2019-06-11 01:28:42 -0500

    nodes inherit parent onNodesNeeded (if not specified)

--- src/prototypes/Tree.js
+++ src/prototypes/Tree.js
@@ -1,6 +1,21 @@
 "use strict";
 
-["vcl-ui:Tree", [
+["vcl-ui:Tree", {
+
+	onNodesNeeded: function(parent) { 
+		var node = parent, pname = "_onChildNodesNeeded";
+		while(node && !node.hasOwnProperty(pname)) {
+			node = node._parent;
+		}
+		
+		if(node === parent) return;
+		
+		if(node && node.hasOwnProperty(pname)) {
+			return node.fire(pname.substring(1), [parent]);
+		}
+	}
+ 
+}, [
 
 	
 ]];
