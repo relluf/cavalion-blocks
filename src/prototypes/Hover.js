9ff65fb76e4710d187d307cc46d272fee383f313
Ralph <ralph.kazemier@gmail.com>
2023-09-20 02:35:07 -0600

    adds support for storage-uri - where to store Hover<>-props, based upon embedded blocks components uri

--- src/prototypes/Hover.js
+++ src/prototypes/Hover.js
@@ -1,4 +1,12 @@
+// getStorageKey might solve the problem/challenge with a not being able to specify (specializer) the (automagically created) Hover<>-instance (since it would lead to many implicit bases when dealing with long uri paths) embedding the actual blocks component of interest
+
 [("Container<>.glassy.closex"), {
+	onDispatchChildEvent(component, name, evt, f, arg) {
+		if(name === "click") {
+			this.nextTick(() => this.bringToFront());
+		}
+		return this.inherited(arguments);	
+	},
 	onLoad() {
 		let spec = this.getSpecializer(), uri;
 		
@@ -10,11 +18,23 @@
 			uri = spec;
 		}
 		
-		B.i([uri]).then(c => c.set({
+		B.i([this.vars("storage-uri", uri || this.vars("storage-uri"))]).then(c => c.set({
 			parent: this,
 			zoom: this.vars("zoom") || 1
 		}));
 		
 		return this.inherited(arguments);
+	},
+	overrides:{
+		getStorageKey(key) {
+			const su = this.vars("storage-uri");
+			if(su) {
+				this.print(js.sf("getStorageKey(%s): %s %s", key || "", su, key || ""));
+				
+				return key === undefined ? su : js.sf("%s %s", su, key);
+			}
+			
+			return this.inherited(arguments);
+		}
 	}
 }];
